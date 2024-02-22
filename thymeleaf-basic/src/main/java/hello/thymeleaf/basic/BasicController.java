aacefaf5c4e44b240b5a70ffe549b954adc68bef
jostar222 <whtjdgus911@gmail.com>
2024-02-22 23:06:29 +0900

    [thymeleaf-basic] 유틸리티 객체와 날짜

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -66,6 +67,12 @@ public class BasicController {
         }
     }
 
+    @GetMapping("/date")
+    public String data(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        System.out.println("model = " + model);
+        return "basic/date";
+    }
 
     @Data
     static class User {
