17ce5653614752751f5f580c60228e15c729ac25
jostar222 <whtjdgus911@gmail.com>
2024-02-23 23:15:19 +0900

    [thymeleaf-basic] 블록 - block

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -104,13 +104,6 @@ public class BasicController {
         addUsers(model);
         return "basic/each";
     }
-    private void addUsers(Model model) {
-        List<User> list = new ArrayList<>();
-        list.add(new User("userA", 10));
-        list.add(new User("userB", 20));
-        list.add(new User("userC", 30));
-        model.addAttribute("users", list);
-    }
 
     @GetMapping("/condition")
     public String condition(Model model) {
@@ -122,6 +115,20 @@ public class BasicController {
     public String comments(Model model) {
         model.addAttribute("data", "Spring!");
         return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
     }
 
     @Data
