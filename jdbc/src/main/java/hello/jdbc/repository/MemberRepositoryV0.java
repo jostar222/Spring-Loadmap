dde2755609bbb7043bb5f65b8e1822fb3ca4446d
jostar222 <whtjdgus911@gmail.com>
2024-03-20 22:19:56 +0900

    [jdbc] - JDBC개발 - 조회

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.StaticMessageSource;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -32,8 +33,36 @@ public class MemberRepositoryV0 {
         } finally {
             close(con, pstmt, null);
         }
+    }
 
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
 
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
     }
 
     private void close(Connection con, Statement stmt, ResultSet rs) {
