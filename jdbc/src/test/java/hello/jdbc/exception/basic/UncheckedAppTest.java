c1053c0086c0b0964f04db25c533ce49d7fb3677
jostar222 <whtjdgus911@gmail.com>
2024-03-27 22:14:26 +0900

    [jdbc] - 언체크 예외 활용

--- jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
+++ jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -1,15 +1,31 @@
 package hello.jdbc.exception.basic;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
 import java.sql.SQLException;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 @Slf4j
-public class UncheckedAppTest {
+public class UnCheckedAppTest {
     @Test
     void unchecked() {
         Controller controller = new Controller();
         assertThatThrownBy(() -> controller.request())
-                .isInstanceOf(Exception.class);
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            //e.printStackTrace();
+            log.info("ex", e);
+        }
     }
 
     static class Controller {
@@ -31,23 +47,27 @@ public class UncheckedAppTest {
             throw new RuntimeConnectException("연결 실패");
         }
     }
+
     static class Repository {
         public void call() {
             try {
                 runSQL();
             } catch (SQLException e) {
-                throw new RuntimeSQLException(e);
+                throw new RuntimeException(e);
             }
         }
-        private void runSQL() throws SQLException {
+
+        public void runSQL() throws SQLException {
             throw new SQLException("ex");
         }
     }
+
     static class RuntimeConnectException extends RuntimeException {
         public RuntimeConnectException(String message) {
             super(message);
         }
     }
+
     static class RuntimeSQLException extends RuntimeException {
         public RuntimeSQLException() {
         }
@@ -55,4 +75,5 @@ public class UncheckedAppTest {
             super(cause);
         }
     }
+
 }
